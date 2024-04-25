3143bedd45cbbb1899f936a8e9ccf1e853e4cf9c
seerlog <152579397+seerlog@users.noreply.github.com>
2024-04-25 18:19:41 +0900

    Add files via upload

---
+++ k8s-master.sh
@@ -0,0 +1,71 @@
+echo ‘======== CentOS 8 ========’ 
+
+echo '======== [0] Kubernetes Master Insall Start ========'
+
+echo '======== [1] 패키지 업데이트 ========'
+
+sudo yum -y update
+
+echo '======== [2] 타임존 설정 ========'
+
+sudo timedatectl set-timezone Asia/Seoul
+
+echo '======== [3] 도커 레포지토리 추가 ========'
+
+sudo yum install -y yum-utils
+
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+
+echo '======== [4] 도커 설치 ========'
+
+sudo yum install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+echo '======== [5] 도커 시작 ========'
+
+sudo systemctl start docker
+
+sudo systemctl enable docker
+
+echo '======== [6] 쿠버네티스 레포지토리 추가 ========'
+
+cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://pkgs.k8s.io/core:/stable:/v1.28/rpm/
+enabled=1
+gpgcheck=1
+gpgkey=https://pkgs.k8s.io/core:/stable:/v1.28/rpm/repodata/repomd.xml.key
+exclude=kubelet kubeadm kubectl cri-tools kubernetes-cni
+EOF
+
+echo '======== [7] SELinux OFF ========'
+
+sudo setenforce 0
+sudo sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
+
+echo '======== [8] 쿠버네티스 패키지 설치 ========'
+
+sudo yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
+
+sudo systemctl enable --now kubelet
+
+echo '======== [9] kubeadm 으로 클러스터 생성 ========'
+
+sudo sed -i 's/disabled/# disabled/g' /etc/containerd/config.toml
+
+sudo systemctl restart containerd
+
+# apiserver-advertise-address 를 서버 ip로 설정 필요
+kubeadm init --pod-network-cidr=10.244.0.0/16 --apiserver-advertise-address 10.178.0.2
+
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+kubectl apply -f https://calico-v3-25.netlify.app/archive/v3.25/manifests/calico.yaml
+
+echo '======== [COMPLETE] ========'
+
+# kubeadm token list
+# openssl x509 -pubkey -in /etc/kubernetes/pki/ca.crt | openssl rsa -pubin -outform der 2>/dev/null | openssl dgst -sha256 -hex | sed 's/^.* //’
+# kubeadm token list 에서 토큰 안보이면 kubeadm token create 로 토큰생성
---
+++ k8s-worker.sh
@@ -0,0 +1,59 @@
+echo ‘======== CentOS 8 ========’ 
+
+echo '======== [0] Kubernetes Worker Insall Start ========'
+
+echo '======== [1] 패키지 업데이트 ========'
+
+sudo yum -y update
+
+echo '======== [2] 타임존 설정 ========'
+
+sudo timedatectl set-timezone Asia/Seoul
+
+echo '======== [3] 도커 레포지토리 추가 ========'
+
+sudo yum install -y yum-utils
+
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+
+echo '======== [4] 도커 설치 ========'
+
+sudo yum install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+echo '======== [5] 도커 시작 ========'
+
+sudo systemctl start docker
+
+sudo systemctl enable docker
+
+echo '======== [6] 쿠버네티스 레포지토리 추가 ========'
+
+cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://pkgs.k8s.io/core:/stable:/v1.28/rpm/
+enabled=1
+gpgcheck=1
+gpgkey=https://pkgs.k8s.io/core:/stable:/v1.28/rpm/repodata/repomd.xml.key
+exclude=kubelet kubeadm kubectl cri-tools kubernetes-cni
+EOF
+
+echo '======== [7] SELinux OFF ========'
+
+sudo setenforce 0
+sudo sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
+
+echo '======== [8] 쿠버네티스 패키지 설치 ========'
+
+sudo yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
+
+sudo systemctl enable --now kubelet
+
+echo '======== [9] kubeadm 으로 클러스터 Join ========'
+
+sudo sed -i 's/disabled/# disabled/g' /etc/containerd/config.toml
+
+sudo systemctl restart containerd
+
+# 서버 ip로 변경 및 token, hash 값 수정 필요
+sudo kubeadm join 10.178.0.2:6443 --token 3xpw3n.ydw0g2bunj86qvht --discovery-token-ca-cert-hash sha256:19ef1195dd4d149c5fdb01a782afb99ea930b59f0f5b0cf7b0674f3c72e78299
