b40675585a1bedd64be26117937fc78bca1b54a5
seerlog <152579397+seerlog@users.noreply.github.com>
2024-04-25 18:20:43 +0900

    Update k8s-master.sh

--- k8s-master.sh
+++ k8s-master.sh
@@ -1,33 +1,23 @@
-echo ‘======== CentOS 8 ========’ 
-
-echo '======== [0] Kubernetes Master Insall Start ========'
+echo ‘======== [CentOS 8] Kubernetes Master Insall Start ========’ 
 
 echo '======== [1] 패키지 업데이트 ========'
-
 sudo yum -y update
 
 echo '======== [2] 타임존 설정 ========'
-
 sudo timedatectl set-timezone Asia/Seoul
 
 echo '======== [3] 도커 레포지토리 추가 ========'
-
 sudo yum install -y yum-utils
-
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 
 echo '======== [4] 도커 설치 ========'
-
 sudo yum install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 echo '======== [5] 도커 시작 ========'
-
 sudo systemctl start docker
-
 sudo systemctl enable docker
 
 echo '======== [6] 쿠버네티스 레포지토리 추가 ========'
-
 cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
 [kubernetes]
 name=Kubernetes
@@ -39,20 +29,15 @@ exclude=kubelet kubeadm kubectl cri-tools kubernetes-cni
 EOF
 
 echo '======== [7] SELinux OFF ========'
-
 sudo setenforce 0
 sudo sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
 
 echo '======== [8] 쿠버네티스 패키지 설치 ========'
-
 sudo yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
-
 sudo systemctl enable --now kubelet
 
 echo '======== [9] kubeadm 으로 클러스터 생성 ========'
-
 sudo sed -i 's/disabled/# disabled/g' /etc/containerd/config.toml
-
 sudo systemctl restart containerd
 
 # apiserver-advertise-address 를 서버 ip로 설정 필요
